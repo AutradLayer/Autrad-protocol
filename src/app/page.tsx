69436f467e1a13339fdd24397ab6a074f87c3cd6
AutradLayer <cikurfahmi732@gmail.com>
2026-07-04 13:06:22 +0700

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import Hero from "@/components/Hero";
+import Features from "@/components/Features";
+import ComputeMarkets from "@/components/ComputeMarkets";
+import SettlementOverview from "@/components/SettlementOverview";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Features />
+      <ComputeMarkets />
+      <SettlementOverview />
+    </>
+  );
+}
